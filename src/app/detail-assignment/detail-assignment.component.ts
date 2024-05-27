914cee2aa97fd2b764609ec5a0fb7d12eb30bbf5
lafatra <lafatraravelojaona5@gmail.com>
2024-05-27 05:22:36 +0300

    formulaire d'annotation

--- src/app/detail-assignment/detail-assignment.component.ts
+++ src/app/detail-assignment/detail-assignment.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-detail-assignment',
   standalone: true,
-  imports: [],
+  imports:  [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, MatButtonModule, MatCardModule],
   templateUrl: './detail-assignment.component.html',
   styleUrl: './detail-assignment.component.css'
 })
